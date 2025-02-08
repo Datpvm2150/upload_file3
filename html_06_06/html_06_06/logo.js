f69009ef6e26a5c1d5ec27f63e8aafce0bd44595
Datpvm2150 <phamvanmanhdat25102005@gmail.com>
2025-02-09 00:58:48 +0700

    HTML & CSS

---
+++ html_06_06/html_06_06/logo.js
@@ -0,0 +1,6 @@
+var canvas=document.querySelector("canvas");
+canvas.width=100;
+canvas.height=50;
+var ctx=canvas.getContext("2d");
+ctx.font="italic 50px Brush Script MT, cursive";
+ctx.fillText('Snow',10,30);
